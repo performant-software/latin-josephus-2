9fea5398c95aee8a13e0d3b6c4c958fa1e2dfdeb
Sara Stanton <sara.mpa13@gmail.com>
2021-11-03 03:39:25 -0500

    Default to light theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -18,7 +18,7 @@ let setTheme = (theme) =>  {
     document.documentElement.removeAttribute("data-theme");
   }
   localStorage.setItem("theme", theme);
-  
+
   // Updates the background of medium-zoom overlay.
   if (typeof medium_zoom !== 'undefined') {
     medium_zoom.update({
@@ -39,10 +39,7 @@ let transTheme = () => {
 
 let initTheme = (theme) => {
   if (theme == null) {
-    const userPref = window.matchMedia;
-    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
-    }
+    theme = 'light';
   }
   setTheme(theme);
 }
